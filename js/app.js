f2e75c858dadbdf92ceac426173a691e73ec38f4
Nicholas Paro <nichparo@amazon.com>
2019-06-04 15:00:54 -0700

    complete js and basic css styling added

--- js/app.js
+++ js/app.js
@@ -1,39 +1,79 @@
 'use strict';
 
-// Array Demonstration
-// Array is a collection of values
-/*
-Arrays store their values at an index
-an index is a number that starts at 0 and
-increases by one for every new item in ther arra
-y
-The first item ALWAYS starts at index 0
+// Welcome message
+alert('Welcome to the guessing game, please answer with yes/y or no/n');
 
-We access items in an array with bracket notation
-ex: array[2]
-This would be spoken as: "I would access the array at position || index 2"
+// Favorite color
+var favColor = prompt('Is your favorite color #201d57?');
+console.log('fav color initial answer : ' + favColor);
+favColor = favColor.toLowerCase(); // set all string characters as lower case
+console.log('fav color after lower : ' + favColor);
+console.log('fav color first letter after lower : ' + favColor[0]);
 
-example array
-['a','b','c']
-has indexes
-0, 1, 2
+if (favColor === 'yes' || favColor === 'y') {
+  alert('That\'s my favorite color too!');
+} else if (favColor === 'no' || favColor === 'n') {
+  alert('I hate you, we aren\'t friends anymore.');
+} else {
+  alert('You didn\'t really try did you...');
+}
 
-*/
+// Favorite food topping
+var favFoodTopping = prompt('Is your favorite food topping Vermont maple syrup?');
+console.log('fav food topping initial answer : ' + favFoodTopping);
+favFoodTopping = favFoodTopping.toLowerCase(); // set all string characters as lower case
+console.log('fav food topping after lower : ' + favFoodTopping);
+console.log('fav food topping first letter after lower : ' + favFoodTopping[0]);
 
-var emptyArray = [];
-var quizAnswers = ['a','b','c','d','e'];
-var arrayOfThings = ['string', {}, ['a'], console.log, undefined, null, 0, 2, true, 'last item of the array'];
+if (favFoodTopping === 'yes' || favFoodTopping === 'y') {
+  alert('That\'s definitely the right choice, Vermont maple syrup is the best!');
+} else if (favFoodTopping === 'no' || favFoodTopping === 'n') {
+  alert('You\'re wrong and should rethink your life choices');
+} else {
+  alert('Again! You didn\'t really try did you...');
+}
 
-console.log('10th thing in array : ' + arrayOfThings[9]);
-console.log('9th index of array : ' + arrayOfThings[9]);
-console.log('Quiz answer @ index 2 is string c : ' + (quizAnswers[2] === 'c'));
-console.log(quizAnswers);
+// Favorite board game
+var favBoardGame = prompt('Is your favorite board game Settler\'s of Catan?');
+console.log('fav board game initial answer : ' + favBoardGame);
+favBoardGame = favBoardGame.toLowerCase();
+console.log('fav board game after lower : ' + favBoardGame);
+console.log('fav board game first letter after lower : ' + favBoardGame[0]);
 
-quizAnswers[2] = 'z';
-console.log(quizAnswers[2] === 'z');
+if (favBoardGame === 'yes' || favBoardGame === 'y') {
+  alert('Catan is the best board game. Hands down, no questions asked');
+} else if (favBoardGame === 'no' || favBoardGame === 'n') {
+  alert('You should have better taste in board games');
+} else {
+  alert('Not again! You didn\'t try at all...');
+}
 
-console.log('I have this many quiz answers: ' + quizAnswers.length);
+// Favorite pet type
+var favPet = prompt('Is your pet a dog?');
+console.log('fav pet initial answer : ' + favPet);
+favPet = favPet.toLowerCase();
+console.log('fav pet after lower : ' + favPet);
+console.log('fav pet first letter after lower : ' + favPet[0]);
 
-console.log('emptyArray : ' + emptyArray);
+if (favPet === 'yes' || favPet === 'y') {
+  alert('Dog\'s are the best!');
+} else if (favPet === 'no' || favPet === 'n') {
+  alert('You should really consider getting a dog');
+} else {
+  alert('You\'ve got one more chance! You didn\'t try at all, again...');
+}
 
-// console.log(arrayOfThings);
+// Favorite music choice
+var favMusic = prompt('Is your favorite music genre Metal?');
+console.log('fav music initial answer : ' + favMusic);
+favMusic = favMusic.toLowerCase();
+console.log('fav music after lower : ' + favMusic);
+console.log('fav music first letter after lower : ' + favMusic[0]);
+
+if (favMusic === 'yes' || favMusic === 'y') {
+  alert('Rock on my friend, rock on!');
+} else if (favMusic === 'no' || favMusic === 'n') {
+  alert('You should be rocking a little harder');
+} else {
+  alert('I told you this was your last chance... there\'s nothing left');
+}
